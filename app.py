78b25c5fa786b4495c4010a270eef8dc34dc7747
kokoko <kurtjayalegado@riccarton.school.nz>
2026-05-29 13:14:28 +1200

    I have made some changes

--- app.py
+++ app.py
@@ -1,32 +1,14 @@
-from flask import Flask, g
+from flask import Flask, g, render_template
 import sqlite3
 
-DATABASE = 'actuals.db'
+DATABASE = 'Database/actuals.db'
 
 app = Flask(__name__)
-
-def get_db():
-    db = getattr(g, '_database', None)
-    if db is None:
-        db = g._database = sqlite3.connect(DATABASE)
-    return db
-
-@app.teardown_appcontext
-def close_connection(exception):
-    db = getattr(g, '_database', None)
-    if db is not None:
-        db.close()
-
 
 
 @app.route('/')
 def home():
-    db = get_db()
-    cursor = db.cursor()
-    sql = "select price, burgers, photo from Customer_order left join burgers on Customer_order.burgers_id = burgers.id"
-    cursor.execute(sql)
-    results = cursor.fetchall()
-    return str(results)
+    return render_template('home.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
