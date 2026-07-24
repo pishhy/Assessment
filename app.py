b5040ccc4f9fd1d56ee01a06300be000371a5d55
kokoko <kurtjayalegado@riccarton.school.nz>
2026-07-24 13:00:30 +1200

    i added layouts to all possible web page I am going to be using and I am trying to make products appear on the page by using flask and sql

--- app.py
+++ app.py
@@ -1,9 +1,30 @@
 from flask import Flask, g, render_template
 import sqlite3
 
-DATABASE = 'Database/actuals.db'
+DATABASE = 'Database/actuallls.db'
 
 app = Flask(__name__)
+
+
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db=getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query,args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+    
 
 
 @app.route('/')
@@ -12,7 +33,24 @@ def home():
 
 @app.route('/menu')
 def menu():
-    return render_template('menus.html')
+    sql = "select burgers, price, photo from products"
+    results= query_db(sql)
+    return render_template('menus.html', results=results)
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/cart')
+def cart():
+    return render_template('cart.html')
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
