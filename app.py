ecb12ec4b494ef33fb950379d3c303d89b20962e
kokoko <kurtjayalegado@riccarton.school.nz>
2026-07-29 10:22:40 +1200

    I am still trying to put images from my sqlite to my webpage

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, g, render_template
 import sqlite3
 
-DATABASE = 'Database/actuallls.db'
+DATABASE = 'actuallls.db'
 
 app = Flask(__name__)
 
@@ -9,7 +9,6 @@ def get_db():
     db = getattr(g, '_database', None)
     if db is None:
         db = g._database = sqlite3.connect(DATABASE)
-        db.row_factory = sqlite3.Row 
     return db
 
 @app.teardown_appcontext
@@ -30,17 +29,15 @@ def home():
 
 @app.route('/menu')
 def menu():
-    sql = """SELECT burgers, price, photo FROM products"""
-    results = query_db(sql)
-    return render_template('menus.html', results=results)
+    sql = """
+    SELECT burgers, price, photo
+    FROM products
+    """
+    burgers = query_db(sql)
 
+    print("RESULT:", burgers)
 
-@app.route("/base/<int:id>")
-def base(id):
-    sql = """SELECT burgers, price, photo FROM products"""
-    result = query_db(sql, (id,), True)
-    return str(result)
-
+    return str(burgers)
 
 @app.route('/contact')
 def contact():
