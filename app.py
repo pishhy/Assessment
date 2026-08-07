f5a5e55548dc1a301edd00dd737e6f0e457984e4
kokoko <kurtjayalegado@riccarton.school.nz>
2026-08-07 12:53:46 +1200

    I managed to put images on my web page menus and designed it. I am now making my layout way better by putting a logo image instead of just a text

--- app.py
+++ app.py
@@ -1,15 +1,22 @@
 from flask import Flask, g, render_template
 import sqlite3
 
-DATABASE = 'actuallls.db'
+
+DATABASE = 'Database/REAL_ASSESSMENT.db'
+
 
 app = Flask(__name__)
+
 
 def get_db():
     db = getattr(g, '_database', None)
     if db is None:
         db = g._database = sqlite3.connect(DATABASE)
+        db.row_factory = sqlite3.Row
     return db
+
+
+
 
 @app.teardown_appcontext
 def close_connection(exception):
@@ -17,21 +24,25 @@ def close_connection(exception):
     if db is not None:
         db.close()
 
+
 def query_db(query, args=(), one=False):
     cur = get_db().execute(query, args)
     rv = cur.fetchall()
     cur.close()
     return (rv[0] if rv else None) if one else rv
 
+
 @app.route('/')
 def home():
     return render_template('home.html')
 
+
 @app.route('/menu')
 def menu():
-    sql = "SELECT burgers, price, photo FROM products"
+    sql = "SELECT burgers, price, ingredients, condiments, photo FROM products WHERE burgers IS NOT NULL AND burgers != ''"
     results = query_db(sql)
     return render_template("menus.html", results=results)
+
 
 
 
@@ -39,13 +50,20 @@ def menu():
 def contact():
     return render_template('contact.html')
 
+
 @app.route('/login')
 def login():
     return render_template('login.html')
+
 
 @app.route('/cart')
 def cart():
     return render_template('cart.html')
 
+
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+
+
